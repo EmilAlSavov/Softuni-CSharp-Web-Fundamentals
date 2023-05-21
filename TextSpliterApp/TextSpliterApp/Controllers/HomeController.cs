a8ba97f1ac37daa80daa31390211f0760fb0b749
Emil <embakks@gmail.com>
2023-05-21 19:39:55 +0300

    Text Splitter App

---
+++ TextSpliterApp/TextSpliterApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSpliterApp.Models;
+using TextSpliterApp.Models.TextSplitModels;
+
+namespace TextSpliterApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		private static TextViewModel Text = new TextViewModel()
+		{
+			SplitedText = " "
+		};
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index(TextViewModel textViewModel)
+		{
+			return View(textViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Split(TextViewModel textViewModel)
+		{
+            if (ModelState.GetFieldValidationState("Text") != Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Valid)
+			{
+				return RedirectToAction("Index", textViewModel);
+			}
+			textViewModel.SplitedText = textViewModel.Text.Replace(" ", Environment.NewLine);
+			return RedirectToAction("Index", textViewModel);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ TextSpliterApp/TextSpliterApp/Models/TextSplitModels/TextViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TextSpliterApp.Models.TextSplitModels
+{
+	public class TextViewModel
+	{
+		[StringLength(30, MinimumLength = 2)]
+		[Required]
+        public string Text { get; set; }
+
+		public string SplitedText { get; set; }
+    }
+}
